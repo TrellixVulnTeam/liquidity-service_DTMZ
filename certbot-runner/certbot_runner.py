73029fb382da1430f3be4b0f5ec55137c42559f1
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-17 00:20:33 +0000

    Adding tarfile member sanitization to extractall()

--- certbot-runner/certbot_runner.py
+++ certbot-runner/certbot_runner.py
@@ -13,7 +13,26 @@ def handler(event, context):
         '/tmp/state.tar'
     )
     with tarfile.open('/tmp/state.tar', 'r:') as tar:
-        tar.extractall('/tmp/config-dir')
+        def is_within_directory(directory, target):
+            
+            abs_directory = os.path.abspath(directory)
+            abs_target = os.path.abspath(target)
+        
+            prefix = os.path.commonprefix([abs_directory, abs_target])
+            
+            return prefix == abs_directory
+        
+        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+        
+            for member in tar.getmembers():
+                member_path = os.path.join(path, member.name)
+                if not is_within_directory(path, member_path):
+                    raise Exception("Attempted Path Traversal in Tar File")
+        
+            tar.extractall(path, members, numeric_owner=numeric_owner) 
+            
+        
+        safe_extract(tar, "/tmp/config-dir")
     certbot.main.main([
         'certonly',
         '--non-interactive',
